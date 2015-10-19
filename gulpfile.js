54b6d37ed66d20cade2d645ef7d605e84965898f
nooooon <hnzero24@gmail.com>
2015-10-19 16:56:04 +0900

    構成変更

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,5 @@
+var htdocsDir = "../../htdocs/sp/";
+
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var pleeease = require('gulp-pleeease');
@@ -10,52 +12,68 @@ var reload      = browserSync.reload;
 var mainBowerFiles = require("main-bower-files");
 var plumber = require("gulp-plumber");
 
+// html
+gulp.task('html', function(){
+    gulp.src('src/**/*.html', {base: 'src'})
+    .pipe(gulp.dest(htdocsDir))
+    .pipe(reload({stream:true}));
+});
+
 // sass
 gulp.task('sass', function () {
     gulp.src('src/sass/**/*.scss')
+        .pipe(plumber())
         .pipe(sass({errLogToConsole: true}))
         .pipe(pleeease({
             autoprefixer: {
-                browsers: ['last 2 versions']
+                browsers: ['last 4 versions']
             }
         }))
-        .pipe(gulp.dest('css'))
+        .pipe(gulp.dest(htdocsDir + 'css'))
         .pipe(reload({stream:true}));
 });
 
-// js-library-uglify
-gulp.task('library-uglify', function(){
+// js-lib
+gulp.task('js-lib', function(){
     var files = mainBowerFiles({checkExistence:true});
     console.log(files);
+    
     gulp.src(files)
-        .pipe(concat('lib.js'))
-        .pipe(uglify())
-        .pipe(gulp.dest('js'));
+        .pipe(gulp.dest(htdocsDir + 'js/libs'));
+        
+    gulp.src(['src/js/libs/*.js'])
+        .pipe(gulp.dest(htdocsDir + 'js/libs'));
 });
 
-// js-concat-uglify
+// js
 gulp.task('js', function() {
-    gulp.src(['src/js/*.js'])
+    gulp.src(['src/js/**/*.js', '!src/js/libs/*.js'])
         .pipe(plumber())
         .pipe(concat('index.js'))
         .pipe(uglify({preserveComments: 'some'}))
-        .pipe(gulp.dest('js'))
+        .pipe(gulp.dest(htdocsDir + 'js'))
         .pipe(reload({stream:true}));
 });
 
 // imagemin
 gulp.task('imagemin', function() {
-    gulp.src(['src/images/**/*.{png,jpg,gif}'])
+    gulp.src(['src/images/**/*.{png,jpg,gif,ico}'])
         .pipe(changed('images'))
         .pipe(imagemin({optimizationLevel: 7}))
-        .pipe(gulp.dest('images'));
+        .pipe(gulp.dest(htdocsDir + 'images'));
+});
+
+// copy (except for css,js,img)
+gulp.task('copy', function(){
+    //gulp.src(['****'], {base: 'src'})
+        //.pipe(gulp.dest(htdocsDir));
 });
 
 // browser sync
 gulp.task('browser-sync', function() {
     browserSync({
         server: {
-            baseDir: "./"
+            baseDir: htdocsDir
         }
     });
 });
@@ -65,9 +83,17 @@ gulp.task('bs-reload', function () {
     browserSync.reload();
 });
 
-gulp.task('default', ['browser-sync', 'library-uglify'], function() {
+
+
+
+gulp.task('default', ['browser-sync', 'js-lib'], function() {
+    gulp.watch('src/**/*.html',['html']);
     gulp.watch('src/sass/**/*.scss',['sass']);
     gulp.watch('src/js/*.js',['js']);
-    gulp.watch('src/images/**/*.{png,jpg,gif}',['imagemin']);
+    gulp.watch('src/images/**/*.{png,jpg,gif,ico}',['imagemin']);
     gulp.watch("*.html", ['bs-reload']);
+});
+
+gulp.task('release', ['html', 'sass', 'js-lib', 'js', 'imagemin'], function(){
+    
 });
