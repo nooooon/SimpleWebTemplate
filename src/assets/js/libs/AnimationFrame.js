bcb1ce0c4acdf0344b1717f74a5d1389c66a9bd6
keishu <hnzero24@gmail.com>
2018-11-29 23:40:01 +0900

    add animationFrame

---
+++ src/assets/js/libs/AnimationFrame.js
@@ -0,0 +1,38 @@
+class AnimationFrame {
+  constructor() {
+    this._updateList = [];
+    this.id = window.requestAnimationFrame(() => this._update());
+  }
+
+  /* 
+    * 実行する関数を追加
+    * @param fn {Function} - 実行する関数
+  */
+  add(fn) {
+    this._updateList.push(fn);
+  }
+
+  /* 
+    * 実行する関数を削除
+    * @param fn {Function} - 削除する関数
+  */
+  remove(fn) {
+    let list = [];
+    for(let val in this._updateList) {
+      if(this._updateList[val] != fn) {
+        list.push(this._updateList[val]);
+      }
+    }
+    this._updateList = list;
+  }
+
+  _update() {
+    for(let [val,i] in this._updateList) {
+      this._updateList[val]();
+    }
+    
+    window.requestAnimationFrame(() => {this._update()});
+  }
+}
+
+module.exports = new AnimationFrame();
